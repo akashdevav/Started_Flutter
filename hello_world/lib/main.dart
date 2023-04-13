5ed5cf5261848bcf46dadd1fa442cd3ff77dd863
akashdevav <akashofficialone@gmail.com>
2023-04-14 03:08:20 +0530

    added colour gradients for two diff colour at top to bottom

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -4,17 +4,25 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        //backgroundColor: Colors.purpleAccent,
         appBar: AppBar(
-            title: const Text('WELCOME BACK'),
-            backgroundColor: const Color.fromARGB(255, 74, 5, 87)),
-        backgroundColor: Colors.purpleAccent,
-        body: const Center(
-          child: Text(
-            'Hello world!',
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
-                fontStyle: FontStyle.italic),
+            title: const Text('WELCOME BACK',
+                style: TextStyle(fontStyle: FontStyle.italic)),
+            backgroundColor: Color.fromARGB(255, 38, 1, 46)),
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 175, 60, 196),
+            Color.fromARGB(255, 70, 5, 82)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          child: const Center(
+            child: Text(
+              'Hello world!',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 254, 253, 253),
+                  fontSize: 30.0,
+                  fontStyle: FontStyle.italic),
+            ),
           ),
         ),
       ),
