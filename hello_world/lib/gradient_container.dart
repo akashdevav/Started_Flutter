791cf3eee2ffe2669ac02955615257534e59367a
akashdevav <akashofficialone@gmail.com>
2023-04-14 07:18:33 +0530

    assigning variables to the object properties(instance variable) and update variable in notes

--- hello_world/lib/gradient_container.dart
+++ hello_world/lib/gradient_container.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/styletext.dart';
 
+//created separate class for container widget
+/*By creating a class some steps that must be followed:
+  -> class name should be uppercase at start position.
+  -> class can extend with some widget.
+  -> inside of the class there must be a constructor.
+  -> finally there is a build function which can return the widget */
+
+
+const aligntop =  Alignment.topLeft;  // defines a variables for the object properties
+const alignbottom = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget{
   //initiate constructor
   const GradientContainer({super.key});
@@ -12,9 +22,9 @@ class GradientContainer extends StatelessWidget{
               gradient: LinearGradient(colors: [
             Color.fromARGB(255, 175, 60, 196),
             Color.fromARGB(255, 70, 5, 82)
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          ], begin: aligntop, end: alignbottom)),
           child: const Center(
-            child: StyleText()
+            child: StyleText('Hello world!')
           ),
         );
   }
--- hello_world/lib/styletext.dart
+++ hello_world/lib/styletext.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
+//created a new class for text widget
 class StyleText extends StatelessWidget {
-  const StyleText({super.key});
+  const StyleText(this.text, {super.key});
+  final text;
+
   @override
   Widget build(context){
-    return  const Text(
-              'Hello world!',
-              style: TextStyle(
+    return   Text(
+              text,
+              style: const TextStyle(
                   color: Color.fromARGB(255, 254, 253, 253),
                   fontSize: 30.0,
                   fontStyle: FontStyle.italic),
